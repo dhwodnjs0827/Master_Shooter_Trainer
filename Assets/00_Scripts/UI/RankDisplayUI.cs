a4eb3f05cdeebf6d406c1559fe05d60596229c0a
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-05-29 21:57:24 +0900

    [Fix] Sprite, Image, RawImage 벌레짓 수정

--- Assets/00_Scripts/UI/RankDisplayUI.cs
+++ Assets/00_Scripts/UI/RankDisplayUI.cs
@@ -6,16 +6,16 @@ public class RankDisplayUI : MonoBehaviour
 {
     [SerializeField] private Text userName;
     [SerializeField] private Text scoreText;
-    [SerializeField] private RawImage weaponImage;
+    [SerializeField] private Image weaponImage;
     [SerializeField] private Text rankText;
-    [SerializeField] private RawImage characterImage;
+    [SerializeField] private Image characterImage;
 
     public void SetUI(int index, DocumentSnapshot data)
     {
         userName.text = data.GetValue<string>("UserName");
         scoreText.text = data.GetValue<int>("BestScore").ToString();
-        weaponImage.texture = ResourceManager.Instance.Load<Texture>($"Sprites/{data.GetValue<string>("Weapon")}");
+        weaponImage.sprite = ResourceManager.Instance.Load<Sprite>($"Sprites/{data.GetValue<string>("Weapon")}");
         rankText.text = index.ToString();
-        characterImage.texture = ResourceManager.Instance.Load<Texture>($"Sprites/{data.GetValue<string>("Character")}");
+        characterImage.sprite = ResourceManager.Instance.Load<Sprite>($"Sprites/{data.GetValue<string>("Character")}");
     }
 }
